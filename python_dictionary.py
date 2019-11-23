64eeac980bbb1a2953cfb0f117dab42dd69a3866
Jossman <jossman@gmail.com>
2019-11-23 10:40:05 +0700

    update commit

--- python_dictionary.py
+++ python_dictionary.py
@@ -5,13 +5,11 @@ data = json.load(open(lok_file))
 
 
 def cari_arti(kata):
+    kata = kata.lower()
     if kata in data:
         return data[kata]
     else:
-        print("kata yang anda cari tidak ditemukan, mohon cek lagi kata-kata anda?")
+        return ("kata yang anda cari tidak ditemukan, mohon cek lagi kata-kata anda?")
 
 
-print(cari_arti{"rain"})
-
-
-
+print(cari_arti("RaiN"))
