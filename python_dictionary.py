00bd4ff2659c7b7ecee387b5ae50bb8d4e9951f2
Jossman <jossman@gmail.com>
2019-11-23 10:51:53 +0700

    update commit

--- python_dictionary.py
+++ python_dictionary.py
@@ -1,10 +1,16 @@
 import json
+import difflib
+from difflib import SequenceMatcher, get_close_matches
 
+# lokasi file
 lok_file = "files/3.1 data.json.json"
 data = json.load(open(lok_file))
 
+# algoritma cari arti
+
 
 def cari_arti(kata):
+    kata = cari_kesamaan(kata)
     kata = kata.lower()
     if kata in data:
         return data[kata]
@@ -12,4 +18,11 @@ def cari_arti(kata):
         return ("kata yang anda cari tidak ditemukan, mohon cek lagi kata-kata anda?")
 
 
-print(cari_arti("RaiN"))
+def cari_kesamaan(kata):
+    kata = get_close_matches(kata, data.keys())[0]
+    return kata
+
+
+masuk = input("Masukkan kata : ")
+
+print(cari_arti(masuk))
