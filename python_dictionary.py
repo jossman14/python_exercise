8a406ac0656b9443639304bd635f40b20fe834e0
Jossman <jossman@gmail.com>
2019-11-23 11:36:11 +0700

    update commit

--- python_dictionary.py
+++ python_dictionary.py
@@ -1,6 +1,8 @@
 import json
 import difflib
 from difflib import SequenceMatcher, get_close_matches
+import random
+
 
 # lokasi file
 lok_file = "files/3.1 data.json.json"
@@ -10,17 +12,31 @@ data = json.load(open(lok_file))
 
 
 def cari_arti(kata):
-    kata = cari_kesamaan(kata)
     kata = kata.lower()
+    kata = cari_kesamaan(kata)
     if kata in data:
-        return data[kata]
+        rand = random.randint(0, len(data[kata]))
+        print(rand)
+        return data[kata][rand]
     else:
-        return ("kata yang anda cari tidak ditemukan, mohon cek lagi kata-kata anda?")
+        return("mohon maaf kata anda tidak ditemukan, silahkan cek kembali kata-kata anda")
 
 
 def cari_kesamaan(kata):
-    kata = get_close_matches(kata, data.keys())[0]
-    return kata
+    kata = get_close_matches(kata, data.keys())
+    for i in range(0, len(kata)):
+        try:
+            cek = input(
+                f"apakah kata yang anda maksud adalah {kata[i]} ? ketik ya atau tidak : ")
+            cek = cek.lower()
+            cek = get_close_matches(cek, ['ya', "tidak"])[0]
+            if cek == "ya":
+                return kata[i]
+            else:
+                pass
+        except:
+            return("NONONO")
+            break
 
 
 masuk = input("Masukkan kata : ")
