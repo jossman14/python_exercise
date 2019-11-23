732396b8925bcf114c4bd098b8a8489b0fe41c7f
Jossman <jossman@gmail.com>
2019-11-23 11:40:52 +0700

    update commit

--- python_dictionary.py
+++ python_dictionary.py
@@ -15,28 +15,31 @@ def cari_arti(kata):
     kata = kata.lower()
     kata = cari_kesamaan(kata)
     if kata in data:
-        rand = random.randint(0, len(data[kata]))
-        print(rand)
+        rand = random.randint(0, len(data[kata]) - 1)
         return data[kata][rand]
     else:
         return("mohon maaf kata anda tidak ditemukan, silahkan cek kembali kata-kata anda")
 
 
 def cari_kesamaan(kata):
-    kata = get_close_matches(kata, data.keys())
-    for i in range(0, len(kata)):
-        try:
-            cek = input(
-                f"apakah kata yang anda maksud adalah {kata[i]} ? ketik ya atau tidak : ")
-            cek = cek.lower()
-            cek = get_close_matches(cek, ['ya', "tidak"])[0]
-            if cek == "ya":
-                return kata[i]
-            else:
-                pass
-        except:
-            return("NONONO")
-            break
+    if kata in data:
+        return kata
+
+    else:
+        kata = get_close_matches(kata, data.keys())
+        for i in range(0, len(kata)):
+            try:
+                cek = input(
+                    f"apakah kata yang anda maksud adalah {kata[i]} ? ketik ya atau tidak : ")
+                cek = cek.lower()
+                cek = get_close_matches(cek, ['ya', "tidak"])[0]
+                if cek == "ya":
+                    return kata[i]
+                else:
+                    pass
+            except:
+                return("NONONO")
+                break
 
 
 masuk = input("Masukkan kata : ")
